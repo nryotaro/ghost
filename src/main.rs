3dddd9c09be4803628e76ef299e1dd9ad3f5ef8d
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-03-25 02:50:37 +0900

    hi

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,12 @@ fn build_ui(application: &gtk::Application) {
     let window: ApplicationWindow = builder.get_object("window").expect("Couldn't get widnow");
     window.set_application(Some(application));
 
-    let label: Label = builder.get_object("label1").expect("Couldn't get label");
+    // let label: Label = builder.get_object("label1").expect("Couldn't get label");
+    let label: Label = Label::new(Some("doge"));
     let menu: Box = builder.get_object("list").expect("Couldn't get box");
+    menu.add(&label);
     // list
+    // https://palepoli.skr.jp/tips/pygobject/container2.php
     //menu.remove(&label);
     window.show_all();
 }
